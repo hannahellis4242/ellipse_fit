7f9b29f5ceb9af05fc51337116df027892a8e137
Hannah Ellis <hannahellis4242@gmail.com>
2021-04-12 09:46:05 +0100

    translating points to fit the unit circle they would have come from if the parameters are correct. Taking the trace of the translated versions with their own transpose should then result in a number which is twice the number of points. Can use this to create a score function

--- run.cpp
+++ run.cpp
@@ -16,9 +16,8 @@ static boost::numeric::ublas::matrix<double> parse(const std::string & filename)
 	unsigned int line_count = 0;
 	{
 		std::ifstream file(filename.c_str());
-		if( file.is_open() )
+		if( file.good() )
 		{
-
 			//step 1 find out how many lines the file has
 			std::string line;
 			while(std::getline(file,line))
@@ -30,10 +29,10 @@ static boost::numeric::ublas::matrix<double> parse(const std::string & filename)
 	}
 	std::cout << "number of lines :  " << line_count << std::endl;
 	{
-		boost::numeric::ublas::matrix<double> m(2,line_count);
+		boost::numeric::ublas::matrix<double> m(line_count,3);
 		std::ifstream file(filename.c_str());
 		std::cout << "reopening file" << std::endl;
-		if(file.is_open())
+		if(file.good())
 		{
 			std::cout << "is open " << std::endl;
 			for( unsigned int i=0; i < line_count; ++i)
@@ -42,8 +41,9 @@ static boost::numeric::ublas::matrix<double> parse(const std::string & filename)
 				std::getline(file,first,',');
 				std::getline(file,second);
 				std::cout << first << " , "  << second << std::endl;
-				m(0,i)=boost::lexical_cast<double>(first);
-				m(1,i)=boost::lexical_cast<double>(second);
+				m(i,0)=boost::lexical_cast<double>(first);
+				m(i,1)=boost::lexical_cast<double>(second);
+				m(i,2)=1.0;
 			}
 		}
 		file.close();
@@ -67,6 +67,30 @@ EllipseParams(const double A, const double B, const double alpha, const double p
 }
 };
 
+static boost::numeric::ublas::matrix<double> transform(const EllipseParams & p)
+{
+	boost::numeric::ublas::matrix<double> T1 = boost::numeric::ublas::identity_matrix(3,3);
+	T1(2,0)=-p.centre_x;
+	T1(2,1)=-p.centre_y;
+	std::cout << "T1 : " << T1 << std::endl ;
+	
+	boost::numeric::ublas::matrix<double> T2 = boost::numeric::ublas::identity_matrix(3,3);
+	T2(0,0)=T2(1,1)=cos(p.angle);
+	T2(1,0)=sin(p.angle);
+	T2(0,1)=-T2(1,0);
+	std::cout << "T2 : " << T2 << std::endl ;
+	
+	boost::numeric::ublas::matrix<double> T3 = boost::numeric::ublas::identity_matrix(3,3);
+	T3(0,0)=1.0/p.major;
+	T3(1,1)=1/p.minor;
+	std::cout << "T3 : " << T3 << std::endl ;
+	
+	boost::numeric::ublas::matrix<double> out = prod(T1,T2);
+	out = prod(out, T3);
+	std::cout << "product : " << out << std::endl ;
+	return out;
+}
+
 class Point {
 public:
 double x, y;
@@ -83,36 +107,28 @@ static double distance( const Point & a, const Point & b )
 
 static double score( const boost::numeric::ublas::matrix<double> & xs, const EllipseParams &p)
 {
-//step 1 translate
-	const auto translated = translate(xs,make_vector(std::vector<double>{-p.centre_x,-p.centre_y}));
-	std::cout<<"translated : " <<translated<<std::endl;
-	const auto s = sin(p.angle);
-	const auto c = cos(p.angle);
-	const auto R = *make_matrix(2,2,std::vector<double>{c,s,-s,c});
-	std::cout << "R : " << R << std::endl;
-	const boost::numeric::ublas::matrix<double> pointsInEllipseFrame = prod(R,translated);
-	std::cout << pointsInEllipseFrame << std::endl;
-
-	double score = 0;
-	for( size_t col = 0, end = pointsInEllipseFrame.size2(); col < end; ++col)
+	const auto trans = transform(p);
+	const boost::numeric::ublas::matrix<double> ys = prod(xs,trans);
+	
+	const boost::numeric::ublas::matrix<double> ys_trans = boost::numeric::ublas::trans(ys);
+	//trace by hand...annoyingly
+	const size_t rows = ys.size1();
+	double sum = 0 ;
+	const boost::numeric::ublas::matrix<double> yProd = prod(ys,ys_trans);
+	std::cout << "yProd : " << yProd << std::endl ;
+	for( size_t row = 0 ; row != rows ; ++row )
 	{
-		const Point point( pointsInEllipseFrame(0,col),pointsInEllipseFrame(1,col));
-		const double top = point.x*point.x*p.major*p.major*p.minor*p.minor;
-		const double bottom = p.major*p.major*point.y*point.y + p.minor*p.minor*point.x*point.x;
-		const double square = top/bottom;
-		const double x1 = sqrt(square);
-		const double x2 = -sqrt(square);
-		const Point intersect1(x1,point.y*x1/point.x);
-		const Point intersect2(x2,point.y*x2/point.x);
-		score += std::min( distance(intersect1, point ), distance(intersect2,point));
+		sum += yProd(row,row);
 	}
-	return score;
+	const double diff = sum - 2*rows;
+	return diff*diff;
 }
 
 void run(const std::string & filename)
 {
 	const boost::numeric::ublas::matrix<double> input = parse(filename);
 	std::cout << "input : "<< input << std::endl;
+	EllipseParams params(2,1,45*boost::math::constants::pi<double>()/180.0,1,1);
 	const auto s = score(input,EllipseParams(2,1,45*boost::math::constants::pi<double>()/180.0,1,1));
 	std::cout << "score : " << s << std::endl ;
 }
